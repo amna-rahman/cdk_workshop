f36f2f94a56e0b2bf7f61cee746f1a70e7ff33f2
Amna Rahman <amnarahman00@gmail.com>
2022-12-27 13:06:22 +0500

    Assertion test

--- tests/unit/test_cdk_workshop_stack.py
+++ tests/unit/test_cdk_workshop_stack.py
@@ -1,21 +1,20 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+from aws_cdk import (
+        Stack,
+        aws_lambda as _lambda,
+        assertions
+    )
+from cdk_workshop.hitcounter import HitCounter
+import pytest
 
 
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkWorkshopStack(app, "cdk-workshop")
+def test_dynamodb_table_created():
+    stack = Stack()
+    HitCounter(stack, "HitCounter",
+            downstream=_lambda.Function(stack, "TestFunction",
+                runtime=_lambda.Runtime.PYTHON_3_7,
+                handler='hello.handler',
+                code=_lambda.Code.from_asset('lambda')),
+    )
     template = assertions.Template.from_stack(stack)
+    template.resource_count_is("AWS::DynamoDB::Table", 1)
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
-    })
-
-
-def test_sns_topic_created():
-    app = core.App()
-    stack = CdkWorkshopStack(app, "cdk-workshop")
-    template = assertions.Template.from_stack(stack)
-
-    template.resource_count_is("AWS::SNS::Topic", 1)
